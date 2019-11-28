755ba64cb635a5fc5602215200535d2731e594a4
drystar <chris.drysdale12@gmail.com>
2019-11-28 00:04:46 -0800

    cleaning up

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,97 +1,16 @@
-//import React from "react";
 import React, { useState, useEffect } from "react";
 
 import axios from "axios";
+import "components/Application.scss";
 
 import DayList from "components/DayList";
-import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment";
 
 import { getAppointmentsForDay } from "helpers/selectors";
 import { getInterview } from "helpers/selectors";
 
-import "components/Application.scss";
-
-// const appointments = [
-//   {
-//     id: 1,
-//     time: "12pm",
-//   },
-//   {
-//     id: 2,
-//     time: "1pm",
-//     interview: {
-//       student: "Lydia Miller-Jones",
-//       interviewer: {
-//         id: 1,
-//         name: "Sylvia Palmer",
-//         avatar: "https://i.imgur.com/LpaY82x.png",
-//       }
-//     }
-//   },
-//   {
-//     id: 3,
-//     time: "2pm",
-//     interview: {
-//       student: "Chris Drysdale",
-//       interviewer: {
-//         id: 1,
-//         name: "Jamie Jones",
-//         avatar: "https://i.imgur.com/twYrpay.jpg",
-//       }
-//     }
-//   },
-//   {
-//     id: 4,
-//     time: "3pm",
-//     interview: {
-//       student: "Emma Watts",
-//       interviewer: {
-//         id: 1,
-//         name: "Autumn Fall",
-//         avatar: "https://i.imgur.com/imgur.jpg",
-//       }
-//     }
-//   },
-//   {
-//     id: 5,
-//     time: "4pm",
-//     interview: {
-//       student: "Hilda Lighthosue",
-//       interviewer: {
-//         id: 2,
-//         name: "Jess Apple",
-//         avatar: "https://i.imgur.com/Nmx0Qxo.png",
-//       }
-//     }
-//   }
-
-// ];
-
-
-// const days = [
-//   {
-//     id: 1,
-//     name: "Monday",
-//     spots: 2,
-//   },
-//   {
-//     id: 2,
-//     name: "Tuesday",
-//     spots: 5,
-//   },
-//   {
-//     id: 3,
-//     name: "Wednesday",
-//     spots: 0,
-//   },
-// ]; 
-
-// const setDay = day => setState({ ...state, day });
 
 export default function Application(props) {
-
-  // const [days, setDays] = useState([])
 
   const [state, setState] = useState({
     day: "Monday",
@@ -102,14 +21,20 @@ export default function Application(props) {
   
   const setDay = day => setState({...state, day});
 
-  // const setDays = days => setState(prev => ({...prev, days}));
-  // useEffect(() => {
-  //   axios
-  //     .get('/api/days')
-  //     .then(response => setDays(response.data));
-  // }, []);
-
   const appointments = getAppointmentsForDay(state, state.day);
+
+  const schedule = appointments.map(appointment => {
+    const interview = getInterview(state, appointment.interview);
+
+    return(
+      <Appointment 
+        key={appointment.id}
+        id={appointment.id}
+        time={appointment.time}
+        interview={interview}
+      />
+    );
+  });
 
   useEffect(() => {
     Promise.all([
@@ -128,22 +53,6 @@ export default function Application(props) {
   }, []);
 
 
-  const schedule = appointments.map(appointment => {
-    const interview = getInterview(state, appointment.interview);
-
-    return(
-      <Appointment 
-        key={appointment.id}
-        id={appointment.id}
-        time={appointment.time}
-        interview={interview}
-      />
-    );
-  });
-
- 
-
-
   return (
     <main className="layout">
       <section className="sidebar">
@@ -157,7 +66,6 @@ export default function Application(props) {
         <DayList
           day={state.day}
           days={state.days}
-          // day={"Monday"}
           setDay={setDay}
           />
         </nav>
