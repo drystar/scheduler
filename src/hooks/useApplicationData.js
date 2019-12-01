6ab3464cb67c2a88295cfdb83515bb76f3509a18
drystar <chris.drysdale12@gmail.com>
2019-12-01 14:34:58 -0800

    removing flawed spot logic

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -2,13 +2,9 @@ import { useEffect, useReducer } from "react";
 
 import axios from "axios";
 
-// import { getDayForAppointment } from "helpers/selectors";
-
 const SET_DAY = "SET_DAY";
 const SET_APPLICATION_DATA = "SET_APPLICATION_DATA";
 const SET_INTERVIEW = "SET_INTERVIEW";
-// const SET_SPOTS_PLUS = "SET_SPOTS_PLUS";
-// const SET_SPOTS_MINUS = "SET_SPOTS_MINUS";
 
 function reducer(state, action) {
   switch (action.type) {
@@ -38,38 +34,13 @@ function reducer(state, action) {
         }
       };
     }
+    default: {
+      return state;
+    }
   }
-//     case SET_SPOTS_PLUS: {
-//       const appointmentDay = getDayForAppointment(state, action.id);
-//       return {...state,
-//         state.days.map((item, index) => {
-//         if (item.appointments === id) {
-//           item.spots;
-//           return item++;
-//         } else {
-//           return item;
-//         }
-//       });
-//     }
-//     case SET_SPOTS_MINUS: {
-//       const days = state.days.map((item, index) => {
-//         if (item.appointments === id) {
-//           item.spots;
-//           return item--;
-//         } else {
-//           return item;
-//         }
-//       });
-//     }
-//     default: {
-//       return state;
-//     }
-//   }
-// }
+}
 
-
-
-function useApplicationData() {
+export default function useApplicationData() {
   const [state, dispatch] = useReducer(reducer, {
     day: "Monday",
     days: [],
@@ -113,23 +84,11 @@ function useApplicationData() {
       [id]: appointment
     };
 
-    // const days = state.days.map((day => {
-    //   if (day.appointments === appointments.(id)){ 
-    //     item.spots ++
-    //     return item
-    //   } else {
-    //     return item
-    //   }
-    // })
-
     // setState({
     //   ...state,
     //   appointments
     // });
     return axios.put(`/api/appointments/${id}`, { interview }).then(() => {
-      if (state.appointments[id].interview === null) {
-        // dispatch({ type: SET_SPOTS_MINUS, id, interview });
-      }
       dispatch({ type: SET_INTERVIEW, id, interview });
     });
   }
@@ -145,34 +104,12 @@ function useApplicationData() {
       [id]: nullAppointment
     };
 
-
-// DECORATE DAYS
-// take in array OF days & appointments
-// return array if decorate days 
-// map over each day
-// compute value for each day 
-// return the key 
-
-
-    // const decorateDays = days.map((days, appointemnts) => {
-
-
-    //   if (item.appointments === (id)){
-    //     item.spots --
-    //     return item
-    //   } else {
-    //     return item
-    //   }
-    // })
-
-  
-
     // setState({
     //   ...state,
     //   appointments
     // });
     return axios.delete(`/api/appointments/${id}`).then(() => {
-      // dispatch({ type: SET_SPOTS_PLUS, id, interview: null });
+      dispatch({ type: SET_INTERVIEW, id, interview: null });
     });
   }
 
