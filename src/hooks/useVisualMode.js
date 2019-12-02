71745648e32349f9605a3def541640ad4868c77a
drystar <chris.drysdale12@gmail.com>
2019-12-02 14:24:36 -0800

    creating appointment test & refactoring so all tests pass

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,25 +1,50 @@
+// import { useState } from "react";
+
+// export default function useVisualMode(initial) {
+//   const [mode, setMode] = useState(initial);
+//   const [history, setHistory] = useState([initial]);
+
+//   function transition(newMode, replace = false) {
+//     if (replace) {
+//       history.pop();
+//       setHistory(prev => [...prev, mode]);
+//     } else {
+//       setHistory(prev => [...prev, mode]);
+//     }
+//     setMode(newMode);
+//   }
+
+//   function back() {
+//     if (history.length > 1) {
+//       history.pop();
+//       setMode(history[history.length - 1]);
+//     }
+//   }
+
+//   return { mode, transition, back };
+// }
+
 import { useState } from "react";
 
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
 
-  function transition(newMode, replace = false) {
+  function transition(mode, replace = false) {
     if (replace) {
-      history.pop();
-      setHistory(prev => [...prev, mode]);
+      setMode(mode);
     } else {
-      setHistory(prev => [...prev, mode]);
+      setMode(mode);
+      setHistory([...history, mode]);
     }
-    setMode(newMode);
   }
-
   function back() {
-    if (history.length > 1) {
-      history.pop();
-      setMode(history[history.length - 1]);
+    if (history.length === 1) {
+      setMode(initial);
+    } else {
+      setMode(history[history.length - 2]);
+      setHistory(history.slice(0, -1));
     }
   }
-
   return { mode, transition, back };
 }
