cb33db7dcd859cbdae4d2e86f4d0bbe763b231d4
drystar <chris.drysdale12@gmail.com>
2019-12-04 11:22:20 -0800

    removing planning comments

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -7,16 +7,6 @@ describe("Appointments", () => {
     cy.contains("Monday");
   });
 
-  // Booking
-  // The plan for testing the booking of an interview is more manageable.
-
-  // Visits the root of our web server
-  // Clicks on the "Add" button in the second appointment
-  // Enters their name
-  // Chooses an interviewer
-  // Clicks the save button
-  // Sees the booked appointment
-
   it("should book an interview", () => {
     cy.get("[alt=Add]")
       .first()
@@ -30,15 +20,6 @@ describe("Appointments", () => {
     cy.contains(".appointment__card--show", "Lydia Miller-Jones");
     cy.contains(".appointment__card--show", "Sylvia Palmer");
   });
-
-  // Editing
-  // If we edit the existing appointment booked for "Archie Cohen", then we don't need to create an appointment first.
-
-  // Visits the root of our web server
-  // Clicks the edit button for the existing appointment
-  // Changes the name and interviewer
-  // Clicks the save button
-  // Sees the edit to the appointment
 
   it("should edit an interview", () => {
     cy.get("[alt=Edit]")
@@ -55,14 +36,6 @@ describe("Appointments", () => {
     cy.contains(".appointment__card--show", "Lydia Miller-Jones");
     cy.contains(".appointment__card--show", "Tori Malcolm");
   });
-
-  // Canceling
-  // We can also perform a test to cancel an existing interview. It is for this reason that we need to reset the database after each test. If one test cancels and interview and the next test expects that interview to exist, then our tests can break for reasons unrelated to our code quality.
-
-  // Visits the root of our web server
-  // Clicks the delete button for the existing appointment
-  // Clicks the confirm button
-  // Sees that the appointment slot is empty
 
   it("should cancel an interview", () => {
     cy.get("[alt=Delete]").click({ force: true });
